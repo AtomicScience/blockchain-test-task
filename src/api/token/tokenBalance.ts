99c912df148119945f1104d30f3ad60026b8d3d1
AtomicScience <dmitry.greblyuk@mail.ru>
2021-06-21 17:34:54 +0300

    TODO updated

--- src/api/token/tokenBalance.ts
+++ src/api/token/tokenBalance.ts
@@ -1,7 +1,6 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import Token from "./token";
 
-// TODO: Research if it is possible to deal without this ugly class
 export default interface TokenBalance {
     token: Token;
     balance: BigNumber;
--- src/api/token/tokenPrice.ts
+++ src/api/token/tokenPrice.ts
@@ -1,6 +1,5 @@
 import Token from "../token/token"
 
-// TODO: Research if it is possible to deal without this ugly class
 export default interface TokenPrice {
     queryStatus : string;
     token : Token;
