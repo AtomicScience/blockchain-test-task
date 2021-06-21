3f16134ac454c11c0a6476f064c36b5cf4da5ac5
AtomicScience <dmitry.greblyuk@mail.ru>
2021-06-21 17:25:40 +0300

    TODO updated

--- src/api/token/tokenBalance.ts
+++ src/api/token/tokenBalance.ts
@@ -1,6 +1,7 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import Token from "./token";
 
+// TODO: Research if it is possible to deal without this ugly class
 export default interface TokenBalance {
     token: Token;
     balance: BigNumber;
